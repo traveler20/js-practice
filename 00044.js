ef440d932e4abb52baffd5cbe2b27719e858d7ed
yurukei20 <60683849+traveler20@users.noreply.github.com>
2021-01-27 08:54:43 +0900

    変数の宣言

---
+++ 00044.js
@@ -0,0 +1,5 @@
+const animals = [{ emoji: "🐶" }, { emoji: "🐱" }];
+const newAnimals = animals.map((value) => value);
+
+console.log(animals); // [{ emoji: "🐶" }, { emoji: "🐱" }]
+console.log(newAnimals); // [{ emoji: "🐶" }, { emoji: "🐱" }]
