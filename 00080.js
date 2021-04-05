775487ec7e0bd6ddbc59976e559607ddb07ad098
yurukei20 <60683849+traveler20@users.noreply.github.com>
2021-04-05 20:22:53 +0900

    console.logの扱い

---
+++ 00080.js
@@ -0,0 +1,6 @@
+console.log(1 == "01"); // true
+console.log("" == 0); // true
+console.log(0 == false); // true
+console.log("" == false); // true
+console.log([1, 2] == "1,2"); // true
+console.log(null == undefined); // true
